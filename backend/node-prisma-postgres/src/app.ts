d45f55a3ace96048cd8cec805993350c5d3111d9
Vivek Prasad <vivekprasad1410@gmail.com>
2024-05-19 13:03:16 +0530

    Revert: secure cookies

--- backend/node-prisma-postgres/src/app.ts
+++ backend/node-prisma-postgres/src/app.ts
@@ -27,7 +27,7 @@ app.use(
     cookie: {
       maxAge: 1000 * 60 * 60 * 24 * 7,
       httpOnly: true,
-      // secure: process.env.NODE_ENV === "production",
+      secure: process.env.NODE_ENV === "production",
     },
     secret: process.env.SESSION_SECRET!,
     saveUninitialized: false,
